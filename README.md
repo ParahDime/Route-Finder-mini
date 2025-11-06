2e4a02ac45a1efdfc9297965c9259fbc776312d2
Parah <122792967+ParahDime@users.noreply.github.com>
2025-11-06 19:12:50 +0000

    updated main.py

---
+++ README.md
@@ -0,0 +1,2 @@
+Route finder mini
+
--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 #packages
 
-
+#used to verify if the file is read or exit (loop)
+def verifyUse(min, max):
+    #has to return 0 or 1
+    return 0
 def verifyFIle():
     a = 0
 def readFile():
     a = 1
 #text menu that calls each module
 
+
 #focus on output
 
 #initialise variables
 
 #ask for type of file read in
+print("Route explorer")
+print("===========\n")
+print("1 | Load Dataset")
+print("0 | Exit")
 
+if(verifyUse(0, 1) == 1):
+    pass
+else:
+    quit()
 #read in files
 
