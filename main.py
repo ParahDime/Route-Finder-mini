8310d67c579759da7f604757bb181d279a075068
Parah <122792967+ParahDime@users.noreply.github.com>
2025-11-17 20:51:21 +0000

    updated verifyUse function

--- main.py
+++ main.py
@@ -2,12 +2,33 @@
 
 #used to verify if the file is read or exit (loop)
 def verifyUse(min, max):
-    #has to return 0 or 1
-    return 0
-def verifyFIle():
-    a = 0
+    while True: #will loop infinitely until the condition is met
+        num = input()
+        try:
+            num = int(num)
+        except ValueError:
+            print("Not a valid value. Please try again")
+            
+        if min <= num and max >= num:
+            return num
+        else:
+            print("Not a valid input. Please try again")
+
+
+def selectFile():
+    pass
+
+def verifyFIle(name):
+    pass
+
+
+
 def readFile():
     a = 1
+
+
+
+
 #text menu that calls each module
 
 
@@ -22,6 +43,12 @@ print("1 | Load Dataset")
 print("0 | Exit")
 
 if(verifyUse(0, 1) == 1):
+    print("Please choose a file type:")
+    print("1 | Small\n2 | Medium\n3 | Large\n\n0 | Quit")
+    fileType = verifyUse(0, 3)
+
+    fileName = "map_" + " " + ".txt"
+    verifyFIle(fileName)
     pass
 else:
     quit()
